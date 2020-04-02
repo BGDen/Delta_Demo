388edc06efcc1cb90b7acf97704ce30ef3c45bab
Vladimir <emirwtf@gmail.com>
2020-04-02 21:43:56 +0300

    first commit

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,7 +5,7 @@ export function Navigation (props){
   return(
     <nav className="navbar navbar-light bg-light">
       
-      <a className="navbar-brand" href="#">DeltaShoppy</a>
+      <a className="navbar-brand" href="#">Delta Shoppy</a>
 
       <div className="form-inline my-2 my-lg-0">
         <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" />
